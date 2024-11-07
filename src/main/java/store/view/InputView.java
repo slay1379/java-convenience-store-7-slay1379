defd2ada064247d5389f1aab383f3aba104d1772
slay <palgalow@gmail.com>
2024-11-07 17:05:31 +0900

    feat(InputView): 멤버십 할인 여부 입력 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -5,9 +5,15 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     private static final String INPUT_PRODUCT_NAME_QUANTITY_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+    private static final String INPUT_MEMBERSHIP_DISCOUNT = "멤버십 할인을 받으시겠습니까? (Y/N)";
 
     public String readProductNameAndQuantity() {
         System.out.println(INPUT_PRODUCT_NAME_QUANTITY_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String readMembershipDiscount() {
+        System.out.println(INPUT_MEMBERSHIP_DISCOUNT);
         return Console.readLine();
     }
 }
