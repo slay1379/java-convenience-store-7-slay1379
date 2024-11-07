bab5f94c9d3812533a3778ebbfb4e2e6f5ad6f9c
slay <palgalow@gmail.com>
2024-11-07 21:37:30 +0900

    feat(Exception): 올바르지 않은 형식 입력 메세지 추가

--- src/main/java/store/exception/MessageConstants.java
+++ src/main/java/store/exception/MessageConstants.java
@@ -2,5 +2,7 @@ package store.exception;
 
 public class MessageConstants {
 
-    private static final String ERROR = "[ERROR]";
+    public static final String ERROR = "[ERROR] ";
+    public static final String PATTERN_EXCEPTION = "올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
+
 }
