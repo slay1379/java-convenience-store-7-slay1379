fbc4e778a9eba6679554d216fc69e098a42a6463
slay <palgalow@gmail.com>
2024-11-07 16:21:09 +0900

    feat(OutputView): 인삿말 출력 기능 추가

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,10 @@
+package store.view;
+
+public class OutputView {
+
+    private static final String GREETING_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.";
+
+    public void printGreeting() {
+        System.out.println(GREETING_MESSAGE);
+    }
+}
