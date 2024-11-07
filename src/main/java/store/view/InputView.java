d60945e8e861cb1acbcdd191f81efdc21c4c0cc9
slay <palgalow@gmail.com>
2024-11-07 17:07:44 +0900

    feat(InputView): 다른 상품 구매 여부 입력 기능 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String INPUT_PRODUCT_NAME_QUANTITY_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String INPUT_MEMBERSHIP_DISCOUNT = "멤버십 할인을 받으시겠습니까? (Y/N)";
+    private static final String INPUT_PURCHASE_OTHER_PRODUCT = "감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)";
 
     public String readProductNameAndQuantity() {
         System.out.println(INPUT_PRODUCT_NAME_QUANTITY_MESSAGE);
@@ -14,6 +15,11 @@ public class InputView {
 
     public String readMembershipDiscount() {
         System.out.println(INPUT_MEMBERSHIP_DISCOUNT);
+        return Console.readLine();
+    }
+
+    public String readPurchaseOtherProduct() {
+        System.out.println(INPUT_PURCHASE_OTHER_PRODUCT);
         return Console.readLine();
     }
 }
