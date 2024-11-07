52b883cf6d32c77af528258b2b8498654d4b0036
slay <palgalow@gmail.com>
2024-11-07 23:42:40 +0900

    feat(MessageConstants): 에러 메세지 추가

--- src/main/java/store/exception/MessageConstants.java
+++ src/main/java/store/exception/MessageConstants.java
@@ -6,5 +6,6 @@ public class MessageConstants {
     public static final String PATTERN_EXCEPTION = "올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
     public static final String FILE_READ_EXCEPTION = "파일 읽기 오류";
     public static final String FILE_FORM_EXCEPTION = "잘못된 형식";
-    public static final String PRODUCT_NAME_EMPTY = "상품명은 공백일 수 없습니다.";
+    public static final String INPUT_PRODUCT_NAME_EMPTY_EXCEPTION = "상품명은 공백일 수 없습니다.";
+    public static final String INPUT_PRODUCT_QUANTITY_NOT_NUMBER_EXCEPTION = "상품 수량은 숫자여야 합니다.";
 }
