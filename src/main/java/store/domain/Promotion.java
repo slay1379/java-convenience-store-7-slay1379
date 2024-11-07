64c0ed7ff84db8b7dfdfbfcb2b64bca3b0ce2dc0
slay <palgalow@gmail.com>
2024-11-07 22:11:51 +0900

    feat(Promotion): 프로모션 객체 생성

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,19 @@
+package store.domain;
+
+import java.time.LocalDate;
+
+public class Promotion {
+    private String name;
+    private int buy;
+    private int get;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
