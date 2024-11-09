2e788e5a067a18688de122fcaf3d66768a06acb1
slay <palgalow@gmail.com>
2024-11-09 23:05:50 +0900

    feat(OutputView): 프로모션 정보 반환 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -7,7 +7,7 @@ public class OutputView {
 
     private static final String GREETING_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.";
 
-    public void printProducts(List<Product> products) {
+    public void printAllProducts(List<Product> products) {
         System.out.println(GREETING_MESSAGE);
         for (Product product : products) {
             printProduct(product);
