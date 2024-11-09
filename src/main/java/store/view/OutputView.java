f2a1f9a6bf84dc1de1768ed5d518f65bf07ed3a8
slay <palgalow@gmail.com>
2024-11-09 22:58:33 +0900

    feat(OutputView): 재고 반환 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,10 +1,27 @@
 package store.view;
 
+import java.util.List;
+import store.domain.Product;
+
 public class OutputView {
 
     private static final String GREETING_MESSAGE = "안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.";
 
-    public void printGreeting() {
+    public void printProducts(List<Product> products) {
         System.out.println(GREETING_MESSAGE);
+        for (Product product : products) {
+            printProduct(product);
+        }
+    }
+
+    private void printProduct(Product product) {
+
+    }
+
+    private String getStockInfo(int stock) {
+        if (stock > 0) {
+            return stock + "개";
+        }
+        return "재고 없음";
     }
 }
