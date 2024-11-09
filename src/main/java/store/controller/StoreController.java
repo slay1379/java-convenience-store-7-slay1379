c2504f4a99ae17c13d64f3b9bd24da4fb2d0fd0d
slay <palgalow@gmail.com>
2024-11-09 23:17:38 +0900

    feat(StoreController): 모든 상품 출력 기능 추가

---
+++ src/main/java/store/controller/StoreController.java
@@ -0,0 +1,35 @@
+package store.controller;
+
+import java.util.List;
+import store.domain.Product;
+import store.domain.Promotion;
+import store.service.InventoryService;
+import store.service.OrderService;
+import store.service.PromotionService;
+import store.view.InputView;
+import store.view.OutputView;
+
+public class StoreController {
+    private InventoryService inventoryService;
+    private PromotionService promotionService;
+    private OrderService orderService;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public StoreController(List<Product> products, List<Promotion> promotions) {
+        this.inventoryService = new InventoryService(products);
+        this.promotionService = new PromotionService(promotions);
+        this.orderService = new OrderService(inventoryService, promotionService);
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+    }
+
+    private void displayProducts() {
+        List<Product> products = inventoryService.getAllProducts();
+        outputView.printAllProducts(products);
+    }
+
+}
