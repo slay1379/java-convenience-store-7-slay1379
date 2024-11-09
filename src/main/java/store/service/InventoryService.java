24120e3d4d4dcfa5df1d561b727d17c331aecb6f
slay <palgalow@gmail.com>
2024-11-09 21:58:22 +0900

    feat(InventoryService): 재고관리 서비스 생성

---
+++ src/main/java/store/service/InventoryService.java
@@ -0,0 +1,35 @@
+package store.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import store.domain.Product;
+import store.domain.Promotion;
+
+public class InventoryService {
+    private Map<String, Product> products;
+
+    public InventoryService(List<Product> productList) {
+        products = new HashMap<>();
+        addProducts(productList);
+    }
+
+    private void addProducts(List<Product> productList) {
+        for (Product product : productList) {
+            products.put(product.getName(), product);
+        }
+    }
+
+    public Product getProduct(String name) {
+        return products.get(name);
+    }
+
+    public void reduceStock(String name, int quantity) {
+        Product product = getProduct(name);
+        product.reduceStock(quantity);
+    }
+
+    public Map<String, Product> getProducts() {
+        return products;
+    }
+}
